a4032be4c9b8c71286732e17b4997fe83a5f4093
Madonna Magdy <madonnamagdy1995@gmail.com>
2022-08-27 14:11:42 +0200

    Add project files.

---
+++ LoginAndStepper/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using LoginAndStepper.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndStepper.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<Title> Titles { get; set; }
+    }
+}
---
+++ LoginAndStepper/Models/Title.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginAndStepper.Models
+{
+    public class Title
+    {
+        public int TitleId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
